482c21af07f1e14a5824dbfb5ba8f9d3b67b8b04
AlbinLindskog <albin.lindskog@live.se>
2020-03-06 10:57:00 +0100

    Refactor WritableNestedMixin

--- drf_expandable_proxy/mixins.py
+++ drf_expandable_proxy/mixins.py
@@ -9,17 +9,21 @@ class WritableNestedMixin:
     the most common (my) use case by delegating to the expanded serializers create
     and update methods.
     """
-
-    def create(self, validated_data):
+    
+    def nested_create(self, validated_data):
         for field_name, field in self.fields.items():
             if not field.read_only and isinstance(field, ExpandableProxy) and field.expanded:
 
                 validated_data[field_name] = self.fields[field_name].create(
                     validated_data=validated_data.pop(field_name)
                 )
-        return super().create(validated_data)
+        return validated_data
 
-    def update(self, instance, validated_data):
+    def create(self, validated_data):
+        validated_data = self.nested_create(validated_data)
+        return super().create(validated_data)
+    
+    def nested_update(self, instance, validated_data):
         for field_name, field in self.fields.items():
             if not field.read_only and isinstance(field, ExpandableProxy) and field.expanded:
 
@@ -27,4 +31,8 @@ class WritableNestedMixin:
                     instance=getattr(instance, field_name),
                     validated_data=validated_data.pop(field_name)
                 )
+        return validated_data
+
+    def update(self, instance, validated_data):
+        validated_data = self.nested_update(instance, validated_data)
         return super().update(instance, validated_data)
